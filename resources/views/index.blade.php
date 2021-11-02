588153137842cd04e6f22e4685c3652586f73fc5
faiqmurtadha <muhammadfaiq29@gmail.com>
2021-11-02 13:03:08 +0700

    Landing Page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" href="logo6.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <title>DaKi | Dasbor Pendaki</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-light" style="background-color: #12372A">
+        <div class="container">
+            <a class="navbar-brand" href="/">
+                <span class="navbar-toogle-icon">
+                    <img src="/logo6.png" alt="logo" width="140" height="140">
+                </span>
+            </a>
+            <span>
+                <a class="nav-link" href="#">Home</a>
+            </span>
+            <span>
+                <a class="nav-link" href="#">Sewa</a>
+            </span>
+            <span id="nav">|</span>
+            <span>
+                <form class="d-flex">
+                    <button class="btn" type="button" href="#">Login</button>
+                </form>
+            </span>
+            <span>
+                <a class="nav-link" href="#" id="navDaftar">Daftar</a>
+            </span>
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <h1 class="h1 display-6">Teman Mendaki Untuk Para<span id="pendaki"> Pendaki&nbsp;</span></h1>
+            </div>
+            <div class="col">
+                <div>
+                    <img class="img-fluid" src="LandingPage.png" alt="LandingPage">
+                </div>
+            </div>
+        </div>
+        <div class="row cf" id="scrolldown">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill ="#C9DB43"class="bi bi-chevron-double-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+        </div>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
