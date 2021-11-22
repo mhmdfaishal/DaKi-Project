13f4e99af48e3df0dd26c9b977d96e945870f582
mhmdfaishal <mhmd.faishal123@gmail.com>
2021-11-22 18:47:21 +0700

    Edit ajax & Add CRUD

---
+++ database/seeders/ProvinceSeeder.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProvinceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Aceh',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Sumatra Utara',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Sumatra Barat',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Riau',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Kep. Riau',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Jambi',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Bengkulu',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Sumatera Selatan',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Kep. Bangka Belitung',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Lampung',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'DKI Jakarta',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Banten',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Jawa Barat',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Jawa Tengah',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Jawa Timur',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'DI Yogyakarta',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Bali',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'NTB',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'NTT',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Kalimantan Barat',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Kalimantan Tengah',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Kalimantan Selatan',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Kalimantan Timur',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Kalimantan Utara',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Sulawesi Utara',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Gorontalo',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Sulawesi Tengah',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Sulawesi Barat',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Sulawesi Selatan',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Sulawesi Tenggara',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Maluku Utara',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Maluku',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Papua Barat',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('provinces')->insert([
+            'nama_provinsi' => 'Papua',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
