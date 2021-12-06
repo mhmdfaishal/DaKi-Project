cca9484cf74fb257ccc3386c60c55c0e0837cca2
mhmdfaishal <mhmd.faishal123@gmail.com>
2021-12-06 14:51:45 +0700

    Merge

--- app/Http/Controllers/BasecampController.php
+++ app/Http/Controllers/BasecampController.php
@@ -184,44 +184,11 @@ class BasecampController extends Controller
             }
         }
     }
-    // public function editGunung($gunung){
-    //     return view('detail_gunung',compact('gunung'));
-    // }
-    // public function updateGunung(Request $request){
-    //     $gambar = $request->file('gambar');
-    //     $name= $gambar->getClientOriginalName();
-    //     $namafile = uniqid();
-    //     $name= substr(md5($namafile), 6, 6) . '_' . time();
-    //     $folder= 'images/gunung';
-    //     $ext = $gambar->getClientOriginalExtension();
-    //     $gambar->move($folder,"$name.$ext");
-    //     $data = Gunung::where('nama_gunung',$request->nama)->update([
-    //         'gambar_gunung' => $name.".".$ext,
-    //         'lokasi' => $request->lokasi,
-    //         'provinsi' => $request->provinsi,
-    //         'status' => $request->status,
-    //         'ketinggian' => $request->ketinggian,
-    //         'htm' => $request->htm,
-    //         'kuota_pendaki' => $request->kuota_pendaki,
-    //         'kontak' => $request->kontak,
-    //         'url_gmaps' => $request->url_gmaps,
-    //     ]);
-    //     if($data){
-    //         return response()->json(['data' => $data,'message'=>'Update Succesfully','status' => true]);
-    //     }else{
-    //         return response()->json(['data' => $data,'message'=>'Update Failed','status' => false]);
-    //     }
-    // }
 
     public function destroyGunung($id){
         $data = Gunung::where('id', $id)->first();
         if($data){
-            // $gambar->storeAs('public/images/gunung/',"$name.$ext");
-            // $data = Gunung::where('id', $request->id_gunung)->first();
             Storage::delete('public/images/gunung/'.$data->gambar_gunung);
-            // if(file_exists(public_path().'/images/gunung/'.$data->gambar_gunung)){
-            //     File::delete(public_path().'/images/gunung/'.$data->gambar_gunung);
-            // }
             $delete = Gunung::where('id', $id)->delete();
             if($delete){
                 return response()->json(['message'=>'Delete Succesfully','status' => true]);
